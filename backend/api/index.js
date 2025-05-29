ddba75c1c9d208b063bc7b29e85d8c2b2d6856a0
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-05-29 18:17:29 +0700

    Added logging patch for debugging serverless function has timed out

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,6 +3,11 @@
 const express = require('express');
 const serverless = require('serverless-http');
 
+console.log('Before Express initialization');
 const app = express();
+console.log('After Express initialization');
 
 module.exports = serverless(app);
+
+console.log('Before exporting serverless handler');
+console.log('After exporting serverless handler');
